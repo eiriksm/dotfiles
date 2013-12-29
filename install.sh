2b5ad1c501ac7ea28390b237fd61f55ac99aba79
Eirik S. Morland <eirik@morland.no>
2013-12-29 14:59:30 +0000

    Add vim plugins and craps.

--- install.sh
+++ install.sh
@@ -16,10 +16,22 @@ echo "Creating $olddir for backup of any existing dotfiles in ~"
 mkdir -p $olddir
 echo "...done"
 
+# install vim craps.
+mkdir -p ~/.vim/colors
+cd ~/.vim/colors
+wget https://raw.github.com/tomasr/molokai/master/colors/molokai.vim
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+mkdir -p ~/.vim/autoload ~/.vim/bundle; curl -Sso ~/.vim/autoload/pathogen.vim     https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+
+
+
+
 # change to the dotfiles directory
 echo "Changing to the $dir directory"
 cd $dir
 echo "...done"
+
+
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
 for file in $dir/*; do
